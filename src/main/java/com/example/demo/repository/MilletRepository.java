8ff733eb34f020085600403167ff0b28e274778c
AkshayaSri Kadiyala <akshayasrikadiyala15@gmail.com>
2026-03-08 18:32:17 +0530

    Update MilletRepository.java

--- src/main/java/com/example/demo/repository/MilletRepository.java
+++ src/main/java/com/example/demo/repository/MilletRepository.java
@@ -1,8 +1,7 @@
 package com.example.demo.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
-
 import com.example.demo.model.Millet;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface MilletRepository extends MongoRepository<Millet, String> {
 }
